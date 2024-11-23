f9be757237a2b6f29ab53963038effb604a6dd81
kche <shiratakekanpakuji@gmail.com>
2024-11-23 15:01:32 +1100

    尝试实现第一个顺序proxy

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
@@ -6,9 +7,158 @@
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+void doit(int fd);
+void read_requesthdrs(rio_t *rp);
+int parse_uri(char *uri, char *filename, char *cgiargs);
+void clienterror(int fd, char *cause, char *errnum, 
+		 char *shortmsg, char *longmsg);
 
-int main()
-{
-    printf("%s", user_agent_hdr);
+int main(int argc, char** argv)
+{    
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    /* Check command line args */
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
+        Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, 
+                    port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+        doit(connfd);                                             //line:netp:tiny:doit
+        Close(connfd);                                            //line:netp:tiny:close
+    }
     return 0;
 }
+
+/*
+ * doit - handle one HTTP request/response transaction
+ */
+/* $begin doit */
+void doit(int fd) 
+{
+    struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char filename[MAXLINE];
+    rio_t rio;
+
+    /* Read request line and headers */
+    Rio_readinitb(&rio, fd);
+    if (!Rio_readlineb(&rio, buf, MAXLINE))  //line:netp:doit:readrequest
+        return;
+    printf("%s", buf);
+    sscanf(buf, "%s %s %s", method, uri, version);       //line:netp:doit:parserequest
+    if (strcasecmp(method, "GET")) {                     //line:netp:doit:beginrequesterr
+        clienterror(fd, method, "501", "Not Implemented",
+                    "Tiny does not implement this method");
+        return;
+    }                                                    //line:netp:doit:endrequesterr
+    read_requesthdrs(&rio);                              //line:netp:doit:readrequesthdrs
+
+    /* Parse URI from GET request */
+    if (stat(filename, &sbuf) < 0) {                     //line:netp:doit:beginnotfound
+	    clienterror(fd, filename, "404", "Not found",
+		    "Tiny couldn't find this file");
+	    return;
+    }                                                    //line:netp:doit:endnotfound
+    
+    /* Request headers */
+    char * Host = "";
+    sscanf(uri, "http://%s/*", Host);
+    Host = strcat("Host: ", Host); 
+    strcpy(buf, Host);
+    strcpy(buf, user_agent_hdr);
+    strcpy(buf, "Connection: close\r\n");
+    strcpy(buf, "Proxy-Connection: close\r\n");
+    
+    Rio_writen(fd, buf, MAXLINE);
+
+}
+/* $end doit */
+
+/*
+ * read_requesthdrs - read HTTP request headers
+ */
+/* $begin read_requesthdrs */
+void read_requesthdrs(rio_t *rp) 
+{
+    char buf[MAXLINE];
+
+    Rio_readlineb(rp, buf, MAXLINE);
+    printf("%s", buf);
+    while(strcmp(buf, "\r\n")) {          //line:netp:readhdrs:checkterm
+	Rio_readlineb(rp, buf, MAXLINE);
+	printf("%s", buf);
+    }
+    return;
+}
+/* $end read_requesthdrs */
+
+/*
+ * parse_uri - parse URI into filename and CGI args
+ *             return 0 if dynamic content, 1 if static
+ */
+/* $begin parse_uri */
+int parse_uri(char *uri, char *filename, char *cgiargs) 
+{
+    char *ptr;
+
+    if (!strstr(uri, "cgi-bin")) {  /* Static content */ //line:netp:parseuri:isstatic
+	strcpy(cgiargs, "");                             //line:netp:parseuri:clearcgi
+	strcpy(filename, ".");                           //line:netp:parseuri:beginconvert1
+	strcat(filename, uri);                           //line:netp:parseuri:endconvert1
+	if (uri[strlen(uri)-1] == '/')                   //line:netp:parseuri:slashcheck
+	    strcat(filename, "home.html");               //line:netp:parseuri:appenddefault
+	return 1;
+    }
+    else {  /* Dynamic content */                        //line:netp:parseuri:isdynamic
+	ptr = index(uri, '?');                           //line:netp:parseuri:beginextract
+	if (ptr) {
+	    strcpy(cgiargs, ptr+1);
+	    *ptr = '\0';
+	}
+	else 
+	    strcpy(cgiargs, "");                         //line:netp:parseuri:endextract
+	strcpy(filename, ".");                           //line:netp:parseuri:beginconvert2
+	strcat(filename, uri);                           //line:netp:parseuri:endconvert2
+	return 0;
+    }
+}
+/* $end parse_uri */
+
+/*
+ * clienterror - returns an error message to the client
+ */
+/* $begin clienterror */
+void clienterror(int fd, char *cause, char *errnum, 
+		 char *shortmsg, char *longmsg) 
+{
+    char buf[MAXLINE];
+
+    /* Print the HTTP response headers */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+
+    /* Print the HTTP response body */
+    sprintf(buf, "<html><title>Tiny Error</title>");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<body bgcolor=""ffffff"">\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "%s: %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<p>%s: %s\r\n", longmsg, cause);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<hr><em>The Tiny Web server</em>\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+}
+/* $end clienterror */
